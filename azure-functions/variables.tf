f6d29a304391246854330b1df05fb4e770e65a08
TJ <tomaszjaronski95@gmail.com>
2023-05-20 22:42:24 +0200

    var storage account

--- azure-functions/variables.tf
+++ azure-functions/variables.tf
@@ -1,11 +1,29 @@
 variable "resource_group_name" {
-  description = "Nazwa grupy zasobów"
+  description = "Name of the resource group."
   type        = string
   default     = "test"
 }
 
 variable "resource_group_location" {
-  description = "Lokalizacja grupy zasobów"
+  description = "Location of the resource group."
   type        = string
   default     = "West Europe"
+}
+
+variable "storage_account_name" {
+  description = "Name of the storage account"
+  type        = string
+  default     = "example-resources"
+}
+
+variable "storage_account_tier" {
+  description = "Tier of the storage account"
+  type        = string
+  default     = "Standard"
+}
+
+variable "storage_account_replication_type" {
+  description = "Replication type of the storage account"
+  type        = string
+  default     = "GRS"
 }
