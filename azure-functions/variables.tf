03205d652eb5b4c8ee99f35e4d7b53fe9f88d0b2
TJ <tomaszjaronski95@gmail.com>
2023-05-20 21:54:02 +0200

    RG

---
+++ azure-functions/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "Nazwa grupy zasobów"
+  type        = string
+  default     = "test"
+}
+
+variable "resource_group_location" {
+  description = "Lokalizacja grupy zasobów"
+  type        = string
+  default     = "West Europe"
+}
