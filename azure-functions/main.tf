b72e901dab9e1a888efd0157684b067bd5df070c
TJ <tomaszjaronski95@gmail.com>
2023-05-20 22:42:09 +0200

    storage account

--- azure-functions/main.tf
+++ azure-functions/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "just_learning" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+resource "azurerm_storage_account" "just_learning" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.resource_group_location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication_type
+}
